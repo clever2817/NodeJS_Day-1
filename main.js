598f608e353655b3d95398dc75ab40695322de62
Menhajul Abir <cleverprogrammer404@gmail.com>
2024-10-08 13:13:48 +0600

    Spread Operator
    Spread Operator

---
+++ main.js
@@ -0,0 +1,7 @@
+let poorCountry = ["Bangladesh", "Srilanka"];
+
+let richCountry = ["America", "Canada", "Italy"];
+
+richCountry = ["America", "Canada", "Italy", ...poorCountry];
+
+console.log(richCountry);
