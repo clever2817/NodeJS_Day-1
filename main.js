dd1898420f3e5fed607c469b54399f2f1eadd76f
Menhajul Abir <cleverprogrammer404@gmail.com>
2024-10-08 13:18:58 +0600

    Array Push and Unshift
    Array Push and Unshift

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
+/// Spread Operator
+
+// let poorCountry = ["Bangladesh", "Srilanka"];
+
+// let richCountry = ["America", "Canada", "Italy"];
+
+// richCountry = ["America", "Canada", "Italy", ...poorCountry];
+
+// console.log(richCountry);
+
+/// Array Push and Unshift
+
 let poorCountry = ["Bangladesh", "Srilanka"];
 
 let richCountry = ["America", "Canada", "Italy"];
 
-richCountry = ["America", "Canada", "Italy", ...poorCountry];
+poorCountry.push(richCountry);
+
+richCountry.unshift(poorCountry);
 
 console.log(richCountry);
+
+console.log(poorCountry);
